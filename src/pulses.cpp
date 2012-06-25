b53fc7f74f2cb64bd8e1a6cfd1ead12ab6795f05
Andrew McWilliams <andy@jahya.net>
2012-06-25 18:03:03 -0400

    Added soundStream setup

--- src/pulses.cpp
+++ src/pulses.cpp
@@ -3,12 +3,22 @@
 void Pulses::setup()
 {
     ofSetDataPathRoot("../../../../data/");
+    
+	int bufferSize = 256;
     bufferCounter = 0;
+    
+	soundStream.listDevices();
+	soundStream.setDeviceID(4);
+	soundStream.setup(this, 0, 2, 44100, bufferSize, 4);
+	
+	channel1.assign(bufferSize, 0.0);
+	channel2.assign(bufferSize, 0.0);
+    
+    calculator.setBufferSize(bufferSize);
 }
 
 void Pulses::audioIn(float * input, int bufferSize, int nChannels){	
 	
-    calculator.setBufferSize(bufferSize);
     calculator.deinterleave(input, channel1, channel2);
 	float nextVolume = calculator.getApproximateVolume(channel1, channel2);
 	volume = calculator.smoothVolume(volume, nextVolume);
@@ -23,7 +33,8 @@ void Pulses::update()
 
 void Pulses::draw()
 {
-    
+    float scaledVolume = ofMap(volume, 0.0, 0.17, 0.0, 1.0, true) * 190.0f;
+    ofCircle(200, 200, scaledVolume);
 }
 
 void Pulses::keyPressed(int key)
