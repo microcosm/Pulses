63234852b9898df39fc5460ec48c2975ac71f0ce
Andrew McWilliams <andy@jahya.net>
2012-06-25 14:36:30 -0400

    Added Calculator class

---
+++ src/calculator.cpp
@@ -0,0 +1,19 @@
+//
+//  calculator.cpp
+//  pulses
+//
+//  Created by Andrew McWilliams on 6/25/12.
+//  www.jahya.net
+//
+
+#include "calculator.h"
+
+float Calculator::rootMeanSquare()
+{
+    return 0.0f;
+}
+
+float Calculator::smooth()
+{
+    return 0.0f;
+}
---
+++ src/calculator.h
@@ -0,0 +1,20 @@
+//
+//  calculator.h
+//  pulses
+//
+//  Created by Andrew McWilliams on 6/25/12.
+//  www.jahya.net
+//
+
+#ifndef pulses_calculator_h
+#define pulses_calculator_h
+
+class Calculator
+{
+    
+public:
+    float rootMeanSquare();
+    float smooth();
+};
+
+#endif
